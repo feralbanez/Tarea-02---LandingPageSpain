8309ea65f950044d188e08bebe32b72b525f9388
Fernando Albanez <ferjose.albanez@gmail.com>
2022-03-25 14:56:21 -0600

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+class BaseConsig(object):
+    SQLALCHEMY_DATABASE_URI = "mysql://fernando4:root@localhost:3306/landingdb"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_SIZE = 20
+    SQLALCHEMY_POOL_TIMEOUT = 300
+    SECRET_KEY = "horrible_secret_key"
